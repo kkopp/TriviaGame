92e1c5ae3243fe3f9e2e60eaa7137eb81698f0c8
kkopp <kristinakopp@Kristinas-MacBook-Pro.local>
2018-10-18 18:13:15 -0600

    added questions object

---
+++ assets/javascript/app.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+
+// Global Variables
+
+
+// Game Questions 
+var triviaQuestions = [{
+    question: "What is a group of bears called?",
+    answers: ["A Den", "A Pack", "A Sleuth", "A Tribe"],
+    correct: "c",
+}, {
+    question: "What is a group of hedgehogs called?",
+    answers: ["An Array", "A Burrow", "A Crowd", "A Den"],
+    correct: "a",
+}, {
+    question: "What is a group of monkeys called?",
+    answers: ["A Barrel", "A Troop", "A Carload", "All of the Above"],
+    correct: "d",
+}, {
+    question: "What is a group of jaguars called?",
+    answers: ["A Pounce", "A Prowl", "A Growl", "A Purr"],
+    correct: "b",
+}, {
+    question: "What is a group of cobras called?",
+    answers: ["An Arsenal", "A Magazine", "A Pit", "A Quiver"],
+    correct: "d",
+}, {
+    question: "What is a group of mice called?",
+    answers: ["A Mischief", "A Scurrying", "A Burrowing", "A Peep"],
+    correct: "a",
+}, {
+    question: "What is a group of ravens called?",
+    answers: ["A Murder", "An Unkindness", "A Smattering", "A Squawk"],
+    correct: "b",
+}, {
+    question: "What is a group of porcupines called?",
+    answers: ["A Quiver", "A Sharpness", "A Prickle", "A Poke"],
+    correct: "c",
+}, {
+    question: "What is a group of giraffes called?",
+    answers: ["A Tower", "A Skyscraper", "A Castle", "A Treetop"],
+    correct: "a",
+}, {
+    question: "What is a group of weasels called?",
+    answers: ["A Cloak", "A Harbor", "A Stash", "A Sneak"],
+    correct: "d",
+}]
+
+// Game Functions
+
+
+
+
+});
