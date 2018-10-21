ce434aaaf4743db6a91439c17c254a6c3c8e108b
kkopp <kristinakopp@Kristinas-MacBook-Pro.local>
2018-10-20 19:05:56 -0600

    started over

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,52 +5,57 @@ $(document).ready(function(){
     // Game Questions 
     var triviaQuestions = [{
         question: "What is a group of bears called?",
-        answers: ["A Den", "A Pack", "A Sleuth", "A Tribe"],
-        correct: "c",
+        choices: ["A Den", "A Pack", "A Sleuth", "A Tribe"],
+        answer: 2,
     }, {
         question: "What is a group of hedgehogs called?",
-        answers: ["An Array", "A Burrow", "A Crowd", "A Den"],
-        correct: "a",
+        choices: ["An Array", "A Burrow", "A Crowd", "A Den"],
+        answer: 1,
     }, {
         question: "What is a group of monkeys called?",
-        answers: ["A Barrel", "A Troop", "A Carload", "All of the Above"],
-        correct: "d",
+        choices: ["A Barrel", "A Troop", "A Carload", "All of the Above"],
+        answer: 3,
     }, {
         question: "What is a group of jaguars called?",
-        answers: ["A Pounce", "A Prowl", "A Growl", "A Purr"],
-        correct: "b",
+        choices: ["A Pounce", "A Prowl", "A Growl", "A Purr"],
+        answer: 1,
     }, {
         question: "What is a group of cobras called?",
-        answers: ["An Arsenal", "A Magazine", "A Pit", "A Quiver"],
-        correct: "d",
+        choices: ["An Arsenal", "A Magazine", "A Pit", "A Quiver"],
+        answer: 3,
     }, {
         question: "What is a group of mice called?",
-        answers: ["A Mischief", "A Scurrying", "A Burrowing", "A Peep"],
-        correct: "a",
+        choices: ["A Mischief", "A Scurrying", "A Burrowing", "A Peep"],
+        answer: 0,
     }, {
         question: "What is a group of ravens called?",
-        answers: ["A Murder", "An Unkindness", "A Smattering", "A Squawk"],
-        correct: "b",
+        choices: ["A Murder", "An Unkindness", "A Smattering", "A Squawk"],
+        answer: 1,
     }, {
         question: "What is a group of porcupines called?",
-        answers: ["A Quiver", "A Sharpness", "A Prickle", "A Poke"],
-        correct: "c",
+        choices: ["A Quiver", "A Sharpness", "A Prickle", "A Poke"],
+        answer: 2,
     }, {
         question: "What is a group of giraffes called?",
-        answers: ["A Tower", "A Skyscraper", "A Castle", "A Treetop"],
-        correct: "a",
+        choices: ["A Tower", "A Skyscraper", "A Castle", "A Treetop"],
+        answer: 0,
     }, {
         question: "What is a group of weasels called?",
-        answers: ["A Cloak", "A Harbor", "A Stash", "A Sneak"],
-        correct: "d",
+        choices: ["A Cloak", "A Harbor", "A Stash", "A Sneak"],
+        answer: 3,
     }]
 
     // Game Functions
     for(var i = 0; i < triviaQuestions.length; i++){
         var triviaQuestionArray = triviaQuestions[i];
-        $(".quizQuestion").text(triviaQuestions[0].question);
-        $(".quizAnswers").html("<button>" + triviaQuestions[0].answers + "</button>");
-        $(".correctAnswer").text(triviaQuestions[0].correct);
+        $(".quizQuestion").text(triviaQuestions[i].question);
+        for(j = 0; j < triviaQuestions[i].answers.length; j++){
+            var btn = document.createElement("button");
+            var t = document.createTextNode(triviaQuestions[i].answers[j])
+            btn.appendChild(t);
+            document.body.appendChild(btn);
+        }
+        $(".correctAnswer").text(triviaQuestions[i].correct);
     }
 
     // when you click the start button, it hides itself //
